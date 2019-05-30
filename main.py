3537f77a0a7e442bc8d48587e78cd1a5272179c6
Zeyu Zhu <zhuzeyu0409@gmail.com>
2019-05-30 10:54:50 +0800

    wechat rebot
    Signed-off-by: Zeyu Zhu <zhuzeyu0409@gmail.com>

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
+# -*- coding: utf-8 -*-
 import itchat
 import urllib,urllib2,json
+import sys,random
+
+defaultencoding = 'utf-8'
+if sys.getdefaultencoding() != defaultencoding:
+    reload(sys)
+    sys.setdefaultencoding(defaultencoding)
+
+num = 6
+url = 'http://www.tuling123.com/openapi/api'
+arr = ["", "", "", "", ""]
+replay = ["来喽", "干啥呀", "你好呀", "在呢", "到"]
 
 @itchat.msg_register(itchat.content.TEXT)
 def get_replay(msg):
-    info = msg['Text'].encode('utf-8')
-    url = 'http://www.tuling123.com/openapi/api'
-    data = {
-        u"key": "xxx",
-        "info": info,
-        u"loc": "",
-        "userid": 'wechat-rebot'
-    }
+    global num
+    info = msg['Text']
+    if info.startswith('胖雨'):
+        if info == '胖雨':
+            print "========>", info
+            itchat.send(replay[random.randint(0,4)], msg['FromUserName'])
+        else:    
+            print "========>", info
+            data = {
+                u"key": arr[num/100],
+                "info": info[2:],
+                u"loc": "",
+                "userid": 'wechat-rebot'
+            }
 
-    data = urllib.urlencode(data)
-    url2 = urllib2.Request(url, data)
-    response = urllib2.urlopen(url2)
+            data = urllib.urlencode(data)
+            url2 = urllib2.Request(url, data)
+            response = urllib2.urlopen(url2)
 
-    apicontent = response.read()
-    s = json.loads(apicontent, encoding='utf-8')
-    print 's==',s
-    if s['code'] == 100000:
-        itchat.send(s['text'], msg['FromUserName'])
+            apicontent = response.read()
+            s = json.loads(apicontent, encoding='utf-8')
+            if s['code'] == 100000:
+                itchat.send(s['text'], msg['FromUserName'])
+                num = num + 1
 
 itchat.auto_login(enableCmdQR=2,hotReload=True)
 itchat.run(debug=True)
